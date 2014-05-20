24c70d2ed3890be3ffc7d0268ebc2cb5a267374b
igoryen <igor.yentaltsev@gmail.com>
2014-05-20 18:07:01 -0400

    first commit

---
+++ src/employee1/Employee1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package employee1;
+
+import java.util.List;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Employee1 {
+    
+    private String name;
+    private int age;
+    List addressList;
+    
+    public String getName(){
+      return name;
+    }
+    //..............
+    //..............
+    //..............
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	
+	
+    }
+    
+}
