060f0e6d9b810c2bb9b4c78123a158d06b9f7360
igoryen <igor.yentaltsev@gmail.com>
2014-05-20 18:24:24 -0400

    Added skeleton methods

--- src/employee1/Employee1.java
+++ src/employee1/Employee1.java
@@ -6,7 +6,9 @@
 
 package employee1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -15,9 +17,40 @@ import java.util.List;
  */
 public class Employee1 {
     
+    private String[] names;
+    private BufferedReader br;
+    
     private String name;
     private int age;
     List addressList;
+    
+    // v1
+    private String init(String[] args){
+	br = new BufferedReader(new InputStreamReader(System.in));
+	String temp = "";
+	if (args.length > 0){
+	    names = new String[args.length];
+	    for (int k = 0; k < args.length; k++){
+		names[k] = args[k];
+	    }
+	    System.out.println();
+	}
+	return temp;
+    }
+    
+    private void run(){
+	
+    }
+    
+    private void usage(){
+	System.out.println("USAGE IS: " + 
+		"WSA500; employee1. Any args ignored.");
+    }
+    
+    private int wrap(){
+	int retval = 0;
+	return retval;
+    }
     
     public String getName(){
       return name;
@@ -32,7 +65,9 @@ public class Employee1 {
     // v2
     public static void main(String[] args) 
     throws IOException {
+	Employee1 theApp = new Employee1();
 	
+	theApp
 	
     }
     
